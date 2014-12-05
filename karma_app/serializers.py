382fc8f070e41e7ded7bb330897422e7ca36716a
nbayaman <nurlan.bayaman@gmail.com>
2014-12-05 00:47:15 -0500

    Disabled CSRF for hitcount ajax, to enable counter for cookiless posts/' views.

--- karma_app/serializers.py
+++ karma_app/serializers.py
@@ -2,9 +2,12 @@ from django.forms import widgets
 from rest_framework import serializers
 from karma_app.models import Post, User, Category
 
-class PostSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    content = serializers.CharField(max_length=140)
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ('id', 'content', 'user', 'category',) 
+
     def create(self, validated_attrs):
         """
         Create and return a new `Snippet` instance, given the validated data.
