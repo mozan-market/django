4c3e044eb9d8f4a914de043169db71dd8a469aea
nbayaman <nurlan.bayaman@gmail.com>
2014-12-16 13:55:03 +0600

    Переделал сериализацию PostSerializer. Добавил сериализацию для категории.

--- mozan_app/serializers.py
+++ mozan_app/serializers.py
@@ -3,30 +3,6 @@ from rest_framework import serializers
 from mozan_app.models import Post, UserProfile, Category, Image, User
 
 
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Image
-        fields = ('post', 'id', 'original_image')
-
-class PostSerializer(serializers.ModelSerializer):
-    # owner = serializers.CharField(source='owner.username', )
-    #user = serializers.PrimaryKeyRelatedField()
-    # Need to solve categories issue. The way they are represented and viewed.
-    # category = serializers.ChoiceField(source='category.name', )
-
-    class Meta:
-        model = Post
-        fields = ('id', 'content', 'owner', 'category', 'images', )
-
-    def create(self, validated_attrs):
-        return Post.objects.create(**validated_attrs)
-
-    def update(self, instance, validated_attrs):
-        instance.content = validated_attrs.get('content', instance.content)
-        instance.save()
-        return instance
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -41,6 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.username', read_only=True)
     class Meta:
@@ -49,6 +26,39 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def create(self, validated_attrs):
         return UserProfile.objects.create(**validated_attrs)
+
+    def update(self, instance, validated_attrs):
+        instance.content = validated_attrs.get('content', instance.content)
+        instance.save()
+        return instance
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Image
+        fields = ('post', 'id', 'original_image')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(required=False)
+    #user = serializers.PrimaryKeyRelatedField()
+    # Need to solve categories issue. The way they are represented and viewed.
+    category = CategorySerializer()
+    images = ImageSerializer()
+
+    class Meta:
+        model = Post
+        fields = ('id', 'content', 'owner', 'category', 'images', )
+
+    def create(self, validated_attrs):
+        return Post.objects.create(**validated_attrs)
 
     def update(self, instance, validated_attrs):
         instance.content = validated_attrs.get('content', instance.content)
